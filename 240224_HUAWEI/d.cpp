07a7bf739c9b55846ef1cd2c294b12171b725edd
tomoakiii <tomoakiii@gmail.com>
2024-02-25 19:37:28 -0500

    0225 2

--- 240224_HUAWEI/d.cpp
+++ 240224_HUAWEI/d.cpp
@@ -6,6 +6,7 @@ const ll INF = 0x0F0F0F0F0F0F0F0F;
 
 int main(){
     int N, M;
+    cin >> N >> M;
     struct node{
         ll l;
         ll d;
@@ -14,23 +15,40 @@ int main(){
         int a;
     };
     vector<vector<node>> v(N);
-    vector<ll> dp(N, INF);
-    dp[N-1] = 0;
+    vector<ll> dp(N, -1);
     node tmp;
     int a, b;
     for(int i=0; i<M; i++){
         cin >> tmp.l >> tmp.d >> tmp.k >> tmp.c >> tmp.a >> b;
+        tmp.a--; b--;
         v[b].push_back(tmp);
     }
-    queue<int> que;
-    que.push(N-1);
+
+    priority_queue<pair<ll, int>> que;
+    que.push({INF, N-1});
     vector<bool> visit(N, false);
     while (!que.empty()){
-        auto q = que.front();
+        auto q = que.top();
         que.pop();
-
-        dp[v[q].a] = dp[q]+v[q].
+        if (dp[q.second] != -1) continue;
+        dp[q.second] = q.first;
+        ll time = q.first;
+        for (auto x: v[q.second]){
+            ll depttime;
+            if (time - x.c > x.l + x.d * (x.k-1)){
+                depttime = x.l + x.d * (x.k-1);
+            } else if (time - x.c < x.l) {
+                depttime = -1;
+            } else {
+                depttime = x.l + x.d * ((time - x.c - x.l)/x.d);
+            }
+            que.push({depttime, x.a});
+        }
     }
-
+    for (int i=0; i<N-1; i++) {
+        if (dp[i] == -1) cout << "Unreachable" << endl;
+        else cout << dp[i] << endl;
+    }
+    return 0;
 }
 
--- 240224_HUAWEI/d2.cpp
+++ 240224_HUAWEI/d2.cpp
@@ -1,51 +1,48 @@
-int main() {
-    using namespace std;
-    unsigned N, M;
-    cin >> N >> M;
-    // 電車の情報を保持する構造体
-    struct train {
-        unsigned from, to;
-        unsigned long first_train;
-        unsigned long interval;
-        unsigned long count;
-        unsigned long distance;
-        // time_limit までに駅 to へ到着するような列車の出発時刻のうち最小のものを求める
-        // 存在しなければ nullopt を返す
-        optional<unsigned long> get_last_train(unsigned long time_limit) const {
-            if (time_limit < first_train + distance)
-                return nullopt;
-            return first_train + min(count - 1, (time_limit - first_train - distance) / interval) * interval;
+#include <bits/stdc++.h>
+#define xx first
+#define yy second
+#define ll long long
+using namespace std;
+
+int main(){
+	ll n,m;
+	cin>>n>>m;
+	vector<vector<array<int,5>>> adj(n+10);
+	
+	for(int i =1;i<=m;i++){
+		int l,d,k,c,a,b;
+		cin>>l>>d>>k>>c>>a>>b;
+		adj[b].push_back({a,l,d,k,c});
+	}
+	
+	vector<ll> dis(n+10,-1);
+	
+	priority_queue<pair<ll,ll>> q;
+	q.push({(ll)4e18,n});
+	
+	while(!q.empty()){
+	
+		auto [dx,x] = q.top();
+		q.pop();
+		
+		if(dis[x]!=-1) continue;
+		dis[x] = dx;
+		
+		for(auto [a,l,d,k,c]:adj[x]){
+			ll m = dx-c;
+			if(m>=l){
+				 m = l + min((m-l)/d,1ll*(k-1))*d;
+				 q.push({m,a});
+			}
+		}
+	}
+	  for (int i = 1; i <= n - 1; i++) {
+        if (dis[i] == -1) {
+             cout << "Unreachable\n";
+        } else {
+            cout << dis[i] << "\n";
         }
-    };
-    vector<vector<train>> train_info(N);
-    for (const auto _ : views::iota(0U, M)) {
-        unsigned long l, d, k, c;
-        unsigned A, B;
-        cin >> l >> d >> k >> c >> A >> B;
-        train_info[--B].push_back({--A, --B, l, d, k, c});
     }
-    priority_queue<pair<unsigned long, unsigned>> pq;
-    vector<unsigned long> ans(N);
-    // 駅 N に十分遅い時間にいるところから探索を始める
-    pq.emplace(ans.back() = numeric_limits<unsigned long>::max(), N - 1);
-    while (!empty(pq)) {
-        // まだ探索していない駅のうちもっとも遅くまでいられる駅を探索する
-        const auto[time_limit, now]{pq.top()};
-        pq.pop();
-        if (ans[now] > time_limit)continue;
-        // 駅 now に向かう電車情報に対して
-        for (const auto &e : train_info[now]) {
-            const auto next{e.from};
-            const auto next_time{e.get_last_train(time_limit)};
-            // 駅 next の最終時刻 ≥ 駅 now の最終時刻に間に合う電車の発射時刻
-            if (next_time && ans[next] < *next_time) // 更新できるなら
-                pq.emplace(ans[next] = *next_time, next); // 優先度つきキューに入れる
-        }
-    }
-    for (const auto d : ans | views::take(N - 1))
-        if (d)
-            cout << d << endl;
-        else
-            cout << "Unreachable" << endl;
-    return 0;
+
+	return 0;
 }
