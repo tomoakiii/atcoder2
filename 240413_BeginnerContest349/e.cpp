0aa7466abfdeb25f9cf75538409e76eb0f729e99
tomoakiii <tomoakiii@gmail.com>
2024-06-16 09:44:49 +0900

    etx

--- 240413_BeginnerContest349/e.cpp
+++ 240413_BeginnerContest349/e.cpp
@@ -6,30 +6,55 @@ const ll INF = 0x0F0F0F0F0F0F0F0F;
 const int INFi = 0x0F0F0F0F;
 
 int main(){
-    vector<vector<ll>> A(3, vector<ll>(3)));
-    for(int i=0; i<3; i++)for(int j=0; j<3; j++){cin>>A[i][j];}    
-    map<vector<vector<ll>>, int> mp;
-    vector<vector<ll>> C(3, vector<ll>(3)));        
+    vector<vector<ll>> A(3, vector<ll>(3));
+    for(int i=0; i<3; i++)for(int j=0; j<3; j++) cin>>A[i][j];    
+    vector<vector<ll>> C(3, vector<ll>(3));
 
-
-    auto [&A]game(auto game, vector<vector<ll>> C, int turn) -> int {
+    auto game = [&A](auto game, vector<vector<ll>> C, int turn) -> int {
+        int aturn = ((turn==1)?2:1);        
+        vector<pair<int, int>> ij;        
         for(int i=0; i<3; i++)for(int j=0; j<3; j++){
-            C2 = C;
-            if (C[i][j]==0) {
-                C2[i][j] = turn;
-                bool isW=false;
-                for(int i2=0; i2<3; i2++){
-                    isW |= (C2[i2][0]==turn) || (C2[i2][1]==turn) || (C2[i2][2]==turn);
-                    isW |= (C2[0][i2]==turn) || (C2[1][i2]==turn) || (C2[2][i2]==turn);
-                }
-                isW |= (C2[0][0]==turn) || (C2[1][1]==turn) || (C2[2][2]==turn);
-                isW |= (C2[0][2]==turn) || (C2[1][1]==turn) || (C2[2][0]==turn);
-                if (isW) return turn;
-                else game(game, C, ((turn==1)?2:1));
-            }            
+            if (C[i][j]==0) ij.push_back({i, j});
         }
-    }
+        int whoW;
+        bool amIwin = false;
+        for(auto i:ij) {
+            auto C2 = C;
+            C2[i.first][i.second] = turn;
+            bool isW=false;
+            for(int i=0; i<3; i++){
+                isW |= ((C2[i][0]==turn) && (C2[i][1]==turn) && (C2[i][2]==turn));
+                isW |= ((C2[0][i]==turn) && (C2[1][i]==turn) && (C2[2][i]==turn));
+            }
+            isW |= ((C2[0][0]==turn) && (C2[1][1]==turn) && (C2[2][2]==turn));
+            isW |= ((C2[0][2]==turn) && (C2[1][1]==turn) && (C2[2][0]==turn));
+            if (isW) {
+                amIwin = true;
+                break;
+            };
+
+            bool isCon = false;
+            ll sm[3] = {0, 0, 0};
+            for(int i=0; i<3; i++)for(int j=0; j<3; j++){
+                if (C[i][j] == 0) {
+                    isCon = true;
+                    break;
+                }
+                sm[C[i][j]]++;
+            }
+            int w;
+            if (!isCon){
+                if(sm[turn] > sm[aturn]) w=turn;
+                else aturn;
+            } else {
+                w = game(game, C, aturn);
+            }
+            if (w==turn) return turn;
+            else continue;            
+        }
+        return aturn;
+    };
     int turn = 1;
-    game(game, C, turn);
+    cout << ((game(game, C, turn)==1)?"Takahashi":"Aoki") << endl;
     return 0;
 }
