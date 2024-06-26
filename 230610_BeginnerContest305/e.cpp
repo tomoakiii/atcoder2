8f03c7016b100e2d2c9eb355204efb464d41a298
tomoakiii <tomotakiii@gmail.com>
2024-06-26 05:26:12 -0400

    240625

--- 230610_BeginnerContest305/e.cpp
+++ 230610_BeginnerContest305/e.cpp
@@ -9,9 +9,34 @@ const ll INF = 0x0F0F0F0F0F0F0F0F;
 const int INFi = 0x0F0F0F0F;
 
 int main(){
-    ll N;
-    cin >> N;
-    vector A(N, 0);
+    ll N, M, K;
+    cin >> N >> M >> K;
+    vector A(N, vector<ll>{});
+    ll a, b;
+    rep(i, M) {
+        cin >> a >> b;
+        a--; b--;
+        A[a].push_back(b);
+        A[b].push_back(a);
+    }
+    vector<ll> P(N);
+    ll mxP = 0, mxi;
+    map<ll, vector<ll>> mp;
+    rep(i, K) {
+        cin >> a >> b;
+        a--;
+        P[a] = b;
+        mp[b].push_back(a);
+        mxP = max(mxP, b);
+    }
+    vector D(N, vector<ll>{});
+    auto que = mp[mxP];
+    
+
+    auto func = [&](auto func, ll from, ll dist){
+
+    }
+    rep(i, )
 
     ll sm = 0;
     rep(i, N) {
