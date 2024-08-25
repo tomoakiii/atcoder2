6914eb6d8af2f9d1f49713a4ca580ed631d95e79
tetitubuta <tomoakiii@gmail.com>
2024-08-24 20:20:06 -0400

    0824

--- 240817_BeginnerContest367/e.cpp
+++ 240817_BeginnerContest367/e.cpp
@@ -11,67 +11,29 @@ const int INFi = 0x0F0F0F0F;
 int main(){
     ll N,K;
     cin >> N >> K;    
-    vector<int> X(N, -1), A(N, 0);
+    vector<int> X(N), A(N, 0);
     rep(i,N) {
-        int z;
-        cin>>z;
-        z--;
-        X[z] = i;
+        cin>>X[i];
+        X[i]--;
     }
     rep(i,N) cin>>A[i];
-
-    
-    vector<bool> visit(N, false);
-    auto f = [&](auto f, int cur)->int{
-        if (X[cur] == -1) return -1;
-        if (visit[cur]) return cur;
-        visit[cur] = true;
-        f(f, X[cur]);
-    };
-    int lp;
-    rep(i,N) if(X[i]!=-1) {
-        lp = f(0, i);
-        if (lp != -1) break;
-    }
-    
-
-    vector<int> lpv;
-    int cntl = 0;
-    vector<bool> visit2(N, false);
-    auto f2 = [&](auto f2, int cur, int stt)->void{
-        lpv.push_back(cur);
-        cntl++;
-        visit2[cur] = true;
-        if (X[cur] == stt) return;        
-        f2(f2, X[cur], stt);
-    };
-    
-    f2(f2, lp, lp);
-
-    vector<int> out(N);
-    rep(i, N) {
-        if (visit2[i]) out[i] = lpv[K%lpv.size()];
-        if (!visit2[i]) {
-            vector<int> tlv;
-            auto f3 = [&](auto f3, int cur)->void{
-                tlv.push_back(cur);
-                cntl++;
-                visit2[cur] = true;
-                if (X[cur] == -1) return;
-                f3(f3, X[cur]);
-            };
-            f3(f3, i);
-            rep(i,tlv.size()){
-                if(i+K < tlv.size()) out[i] = tlv[i+k];
-                else out[i] = tlv[tlv.size()-1];
-            }
+    vector XP(61, vector<int>(N));
+    XP[0] = X;
+    for(int i=1; i<61; i++){
+        vector B = A;
+        rep(j, N) {
+            XP[i][j] = XP[i-1][XP[i-1][j]];
         }
     }
 
-    rep(i, N) {        
-        cout<<A[out[i]]<<" ";
+    rep(k, 61){
+        if ((K>>k) & 1){    
+            vector B = A;
+            rep(i, N) B[i] = A[XP[k][i]];
+            swap(A, B);
+        }
     }
-    
+    rep(i, N) cout << A[i] << " ";
     cout << endl;
     return 0;
 }
--- 240824_BeginnerContest368/e.cpp
+++ 240824_BeginnerContest368/e.cpp
@@ -9,16 +9,37 @@ const ll INF = 0x0F0F0F0F0F0F0F0F;
 const int INFi = 0x0F0F0F0F;
 
 int main(){
-    ll N;
-    cin >> N;
-    vector A(N, 0);
+    ll N, M, X;
+    cin >> N >> M;
+    vector<ll> X(M, 0);
+    cin >> X[0];
+    vector<ll> A(M), B(M), S(M), T(M);
+    vector<int> last(N, -1), lastT(M);
 
-    ll sm = 0;
-    rep(i, N) {
-        cin >> A[i];
-        sm += A[i];
+    // last: last arrival train on each station
+    // lastT: last Need-To-Wait train on each train
+    rep(i,M) {
+        cin>>A[i]>>B[i]>>S[i]>>T[i];
     }
     
+    vector<int> ord_d(M);
+    iota(ord_d.begin(), ord_d.end(), 0);
+    vector ord_a = ord_d;
+    sort(ord_d.begin(), ord_d.end(), [&](int i, int j) { return S[i] < S[j]; });
+    sort(ord_a.begin(), ord_a.end(), [&](int i, int j) { return T[i] < T[js]; });
+
+    int i0=0, i1=0;
+    while(i0 < M && i1 < M){
+        
+        A[i]--, B[i--];
+        last[B[i]] = i;
+        lastT[i] = last[A[i]];
+    }
+
+    auto comp = [](vector&)->bool{
+
+    };
+   
     cout << sm << endl;
     return 0;
 }
