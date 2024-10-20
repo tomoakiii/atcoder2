40e3d25c304f572c4514eb51829ee5a64ea922a4
tomoakiii <tomotakiii@gmail.com>
2024-10-20 09:19:59 -0400

    F

--- 241019_BeginnerContest376/f.cpp
+++ 241019_BeginnerContest376/f.cpp
@@ -11,16 +11,66 @@ const ll INF = 0x0F0F0F0F0F0F0F0F;
 const int INFi = 0x0F0F0F0F;
 
 int main(){
-    ll N;
-    cin >> N;
-    vector A(N, 0);
-
-    ll sm = 0;
-    rep(i, N) {
-        cin >> A[i];
-        sm += A[i];
+    int N, Q;
+    cin >> N >> Q; 
+    vector dp(Q+1, vector<ll>(N, INF));
+    bool lastL = true;
+    dp[0][1] = 0;
+    int curLH = 0, curRH = 1, lastnx = 0;
+    char c;
+    int nx;
+    rep(k, Q){
+        cin >> c >> nx;
+        nx--;
+        rep (i, N) {
+            if (lastL) {
+                curLH = lastnx;
+                curRH = i;
+            } else {
+                curLH = i;
+                curRH = lastnx;
+            }
+            if (dp[k][i] >= INF) continue;
+            ll ofs, ofsR;
+            int mH = 0, sH;
+            if(c=='L') {
+                ofs = (N - curLH)%N;
+                ofsR = (N - ofs)%N;
+                sH = (curRH + ofs)%N;
+            } else {
+                ofs = (N - curRH)%N;
+                ofsR = (N - ofs)%N;
+                sH = (curLH + ofs)%N;
+            }
+            ll nnx = (nx + ofs) % N;
+            ll cnt=0;
+            if (sH <= nnx) {
+                cnt += (nnx - sH + 1);
+                cnt += nnx; 
+                chmin(dp[k+1][(nnx + 1 + ofsR)%N], dp[k][i]+cnt);
+            }
+            if (sH > nnx) {
+                cnt = nnx;
+                chmin(dp[k+1][(sH+ofsR)%N], dp[k][i]+cnt);
+            }
+            cnt = 0;
+            if (sH >= nnx) {
+                cnt += (sH - nnx + 1);
+                cnt += (N-nnx);
+                chmin(dp[k+1][(nnx + (N-1) + ofsR)%N], dp[k][i]+cnt);
+            }
+            if (sH < nnx) {
+                cnt = (N-nnx);
+                chmin(dp[k+1][(sH+ofsR)%N], dp[k][i]+cnt);
+            }            
+        }
+        lastnx = nx;
+        lastL = (c=='L');
     }
+
+    ll ans = INF;
+    rep(i,N) chmin(ans, dp[Q][i]);
+    cout << ans << endl;
     
-    cout << sm << endl;
     return 0;
 }
