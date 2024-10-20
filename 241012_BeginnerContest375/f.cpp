38981735327d797088c86c7113d3201b218675d8
tomoakiii <tomotakiii@gmail.com>
2024-10-20 16:20:20 -0400

    cmt

--- 241012_BeginnerContest375/f.cpp
+++ 241012_BeginnerContest375/f.cpp
@@ -13,29 +13,59 @@ const int INFi = 0x0F0F0F0F;
 int main(){
     ll N, M, Q;
     cin >> N >> M >> Q;
-    vector ab(N, vector<pair<ll, int>>{});
-    rep(i, N) {
+    struct abcst{
+        int a, b;
+        ll c;
+    };
+    vector<abcst> abc(M);
+    rep(i, M) {
         int a, b;
         ll c;
         cin >> a >> b >> c;
         a--, b--;
-        vector[a].emplace_back({c, b});
-        vector[b].emplace_back({c, a});        
+        abc[i].a = a, abc[i].b = b, abc[i].c = c;
     }
-
-    while(Q--){
+    struct qst{
         int q;
-        cin >> q;
-        if (q==1){
-            priority_queue<ll, vector<ll>, greater<ll>>
+        int x, y;
+    };
+    vector<qst> question(Q);
+    set<int> st;
+    rep(i, Q) {
+        cin >> question[i].q >> question[i].x;
+        question[i].x--;
+        if (question[i].q==2) {
+            cin >> question[i].y;
+            question[i].y--;
+        }
+        if (question[i].q==1) st.insert(question[i].x); 
+    }
+    vector dist(N, vector<ll>(N, INF));
+    rep(i,N) dist[i][i]=0;
+    rep(i, M) {
+        if(st.count(i)) continue;
+        abcst ab = abc[i];
+        dist[ab.a][ab.b] = dist[ab.b][ab.a] = ab.c;
+    }
+    rep(k, N) rep(i, N) rep(j, N) chmin(dist[i][j], dist[i][k] + dist[k][j]);
+    vector<ll> ans{};
+    reverse(question.begin(), question.end());
+    rep(x, Q) {
+        qst q = question[x];
+        if (q.q == 1) {
+            abcst ab = abc[q.x];            
+            rep(i, N) rep(j, N) {
+                chmin(dist[i][j], dist[i][ab.a] + ab.c + dist[ab.b][j]);
+                chmin(dist[i][j], dist[i][ab.b] + ab.c + dist[ab.a][j]);
+            }
         } else {
-            int x, y;
-            cin >> x >> y;
-            x--, y--;
-            vector[x].em
+            ans.emplace_back(dist[q.x][q.y]);
         }
     }
-    
-    cout << sm << endl;
+    reverse(ans.begin(), ans.end());
+    for(auto as: ans) {
+        if (as >= INF) as = -1;
+        cout << as << endl;
+    }
     return 0;
 }
