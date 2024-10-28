47c26a42d18661823f2a2cfee4e8a72e3136fd65
tomoakiii <tomotakiii@gmail.com>
2024-10-27 20:17:11 -0400

    cmt

--- abc293/e.cpp
+++ abc293/e.cpp
@@ -10,17 +10,98 @@ typedef long long ll;
 const ll INF = 0x0F0F0F0F0F0F0F0F;
 const int INFi = 0x0F0F0F0F;
 
-int main(){
-    ll N;
-    cin >> N;
-    vector A(N, 0);
 
-    ll sm = 0;
-    rep(i, N) {
-        cin >> A[i];
-        sm += A[i];
+// modint: mod 計算を int を扱うように扱える構造体
+template<int MOD> struct Fp {
+    long long val;
+    constexpr Fp(long long v = 0) noexcept : val(v % MOD) {
+        if (val < 0) val += MOD;
     }
+    constexpr int getmod() { return MOD; }
+    constexpr Fp operator - () const noexcept {
+        return val ? MOD - val : 0;
+    }
+    constexpr Fp operator + (const Fp& r) const noexcept { return Fp(*this) += r; }
+    constexpr Fp operator - (const Fp& r) const noexcept { return Fp(*this) -= r; }
+    constexpr Fp operator * (const Fp& r) const noexcept { return Fp(*this) *= r; }
+    constexpr Fp operator / (const Fp& r) const noexcept { return Fp(*this) /= r; }
+    constexpr Fp& operator += (const Fp& r) noexcept {
+        val += r.val;
+        if (val >= MOD) val -= MOD;
+        return *this;
+    }
+    constexpr Fp& operator -= (const Fp& r) noexcept {
+        val -= r.val;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr Fp& operator *= (const Fp& r) noexcept {
+        val = val * r.val % MOD;
+        return *this;
+    }
+    constexpr Fp& operator /= (const Fp& r) noexcept {
+        long long a = r.val, b = MOD, u = 1, v = 0;
+        while (b) {
+            long long t = a / b;
+            a -= t * b; swap(a, b);
+            u -= t * v; swap(u, v);
+        }
+        val = val * u % MOD;
+        if (val < 0) val += MOD;
+        return *this;
+    }
+    constexpr bool operator == (const Fp& r) const noexcept {
+        return this->val == r.val;
+    }
+    constexpr bool operator != (const Fp& r) const noexcept {
+        return this->val != r.val;
+    }
+    friend constexpr ostream& operator << (ostream &os, const Fp<MOD>& x) noexcept {
+        return os << x.val;
+    }
+    friend constexpr Fp<MOD> modpow(const Fp<MOD> &a, long long n) noexcept {
+        if (n == 0) return 1;
+        auto t = modpow(a, n / 2);
+        t = t * t;
+        if (n & 1) t = t * a;
+        return t;
+    }
+};
+
+
+int main(){
+    ll A, X, M;
+    cin >> A >> X >> M;
+    const int N = 2E6; 
+    ll ans = 1;
+    vector<ll> Ap(N);
+    Ap[0] = A % M;
+    rep(i, N-1) {
+        Ap[i+1] = Ap[i] * Ap[i];
+        Ap[i+1] = Ap[i+1] % M;
+    }    
+    ans = 1;
+    int cnt = 0;
+    while(X){
+        if (X%2) {
+            ans = ans * Ap[cnt];
+            ans = ans % M;
+        }
+        cnt++;
+        X/=2;
+    }
+    ans = ans - 1;
+    if (ans < 0) ans += M;
+    ans = ans % M;
+    ll a = (A-1), b = M, u = 1, v = 0;
+    while (b) {
+        long long t = a / b;
+        a -= t * b; swap(a, b);
+        u -= t * v; swap(u, v);
+    }
+    ans = ans * u % M;
+    if (ans < 0) ans += M;
     
-    cout << sm << endl;
+    cout << ans << endl;
     return 0;
 }
