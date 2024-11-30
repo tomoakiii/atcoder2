b4b4774c1db1c1fe142968baf88d0363a5d1fed0
tetitubuta <tomoakiii@gmail.com>
2024-11-30 18:22:15 -0500

    cmt

--- abc382/f.cpp
+++ abc382/f.cpp
@@ -8,14 +8,28 @@ template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, tr
 
 template <class Type> class LazySegmenttr {
 private:
+    struct val_ind{
+        Type v;
+        int i;
+    };
+    val_ind min(val_ind a, val_ind b) {
+        if (a.v <= b.v) return a;
+        else return b;
+    }
+    val_ind max(val_ind a, val_ind b) {
+        if (a.v >= b.v) return a;
+        else return b;
+    }
+
     int n, sz, trsize;
     struct nd{
         int l;
         int r;
         int c;
-        Type val;
-        int layer;
+        val_ind min, max;
+        Type sum, val;
     };
+
     vector<nd> tr;
 
     // 呼ぶときはgetval(0, index);
@@ -36,13 +50,24 @@ public:
         while(n < sz) n *= 2;
         trsize = 2*n-1;
         tr.resize((size_t)trsize);
-
-        for(int i=0; i<sz; i++)  tr[i+n-1].val = v[i];
+        for(int i=0; i<sz; i++) {
+            tr[i+n-1].sum = tr[i+n-1].max.v = tr[i+n-1].min.v = v[i];
+            tr[i+n-1].max.i = tr[i+n-1].min.i = i;
+        }
         for(int i=0; i<n; i++) tr[i+n-1].l = tr[i+n-1].r = tr[i+n-1].c = i;
+        int k=1;
+        while ((n-1)/k > 0){
+            for(int i=(n-1)/k; i<(trsize-1)/k; i+=2){
+                tr[i/2].max = max(tr[i].max, tr[i+1].max);
+                tr[i/2].min = min(tr[i].min, tr[i+1].min);
+                tr[i/2].sum = tr[i].sum + tr[i+1].sum;
+            }
+            k*=2;
+        }
         for(int i=n-2; i>=0; i--) {      
             tr[i].l = tr[i*2+1].l;
             tr[i].r = tr[i*2+2].r;
-            tr[i].c = (tr[i].l + tr[i].r)/2;            
+            tr[i].c = (tr[i].l + tr[i].r)/2;        
         }
     }
     
@@ -53,7 +78,16 @@ public:
         }
         if(tr[ind].l == a && tr[ind].r == b){
             tr[ind].val += val;
-//            cout << tr[ind].l << "~" << tr[ind].r << endl;
+            chmax(tr[ind].max.v, tr[ind].max.v + val);
+            chmin(tr[ind].min.v, tr[ind].min.v + val);
+            int i = ind;
+            if(i%2 == 0) i--;
+            while(i) {
+                tr[i/2].max = max(tr[i].max, tr[i+1].max);
+                tr[i/2].min = min(tr[i].min, tr[i+1].min);
+                tr[i/2].sum = tr[i].sum + tr[i+1].sum;
+                i /= 2;
+            }
         } else if (tr[ind].c >= a && tr[ind].c < b) {
             add(2*ind+1, a, tr[ind].c, val);
             add(2*ind+2, tr[ind].c + 1, b, val);
@@ -76,23 +110,71 @@ public:
         return getval_main(0, tgt);
     }
 
-    Type getmin() {
-        Type minv = INF;
-        int mind;
-        for(int i = n-1; i < n-1+sz; i++){
-            if(minv > tr[i].val){
-                minv = tr[i].val;
-                mind = i;
-            }
-        }
-        return mind - (n-1);
+    // MaxElement(0, start, end);
+    // 0 is top of segtree. Start and End are number of original array. End is included in search area
+    val_ind MaxElement(int ind, int a, int b){
+        if(tr[ind].l == a && tr[ind].r == b) return tr[ind].max;
+        if (tr[ind].c >= a && tr[ind].c < b) return
+            max(MaxElement(2*ind+1, a, tr[ind].c),
+                MaxElement(2*ind+2, tr[ind].c + 1, b));
+        if (tr[ind].c >= b) return MaxElement(2*ind+1, a, b);
+        return MaxElement(2*ind+2, a, b);   
     }
 
-    Type getsum() {
-        Type sumv = 0;
-        for(int i = n-1; i < n-1+sz; i++) sumv += tr[i].val; 
-        return sumv;
+    // MinElement(0, start, end);
+    // 0 is top of segtree. Start and End are number of original array. End is included in search area
+    val_ind MinElement(int ind, int a, int b){
+        if(tr[ind].l == a && tr[ind].r == b){
+            return tr[ind].min;
+        }
+        if (tr[ind].c >= a && tr[ind].c < b) {
+            return min(MinElement(2*ind+1, a, tr[ind].c),
+                       MinElement(2*ind+2, tr[ind].c + 1, b));
+        }
+        if (tr[ind].c >= b) return MinElement(2*ind+1, a, b);
+        return MinElement(2*ind+2, a, b);
     }
+
+    Type GetSum(int ind, int a, int b){
+        if(tr[ind].l == a && tr[ind].r == b){
+            return tr[ind].val;
+        }
+        if (tr[ind].c >= a && tr[ind].c < b) {
+            return GetSum(2*ind+1, a, tr[ind].c) +
+                GetSum(2*ind+2, tr[ind].c + 1, b);
+        }
+        if (tr[ind].c >= b) return GetSum(2*ind+1, a, b);
+        return GetSum(2*ind+2, a, b);
+    }
+
+    void DispTree() {
+        int ind = 0;
+        int nx = 1;
+        while(ind < trsize) {
+            for(int i=0; i < nx; i++) {
+                cout << ind << "(" << tr[ind].l << "-" << tr[ind].r << "):" << tr[ind].val << ", ";
+                ind++;
+            }
+            cout << endl;
+            nx *= 2;
+        }
+        cout << endl;
+    }
+
+    void DispMaxTree() {
+        int ind = 0;
+        int nx = 1;
+        while(ind < trsize) {
+            for(int i=0; i < nx; i++) {
+                cout << ind << "(" << tr[ind].l << "-" << tr[ind].r << "):" << tr[ind].max.v << ", ";
+                ind++;
+            }
+            cout << endl;
+            nx *= 2;
+        }
+        cout << endl;
+    }
+
 };
 
 typedef long long ll;
@@ -103,17 +185,24 @@ int main(){
     ll H, W, N;
     cin >> H >> W >> N;
     vector<ll> R(N), C(N), L(N);
-    rep(i,N) cin>>R[i]>>C[i]>>L[i];
-    vector st(H+1, vector<int>{});
-    rep(i,N){
-        st[R[i]].emplace_back(i);
+    vector<pair<ll, int>> vp{};
+    rep(i,N) {
+        cin>>R[i]>>C[i]>>L[i];
+        R[i]--, C[i]--;
+        vp.push_back({R[i], i});
     }
-    vector<ll> col(W+1);
+    sort(vp.rbegin(), vp.rend());
+    vector<ll> col(W);
     LazySegmenttr<ll> LS(col);
-    rep(i,H+1){
-        rep(j,st[i].size()){
-            
-        }
+    vector<ll> ans(N);
+    
+    for(auto m: vp){
+        int p = m.second;
+        auto [mx, k] = LS.MaxElement(0, C[p], C[p] + L[p] - 1);
+        LS.add(0, C[p], C[p] + L[p] - 1, 1);
+        ans[p] = H - mx;
+        LS.DispMaxTree();
     }
+    rep(i, N) cout << ans[i] << endl;
     return 0;
 }
