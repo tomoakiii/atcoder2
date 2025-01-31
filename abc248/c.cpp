76b023db7adcfe043e22289b5b85efe5b603df35
tetitubuta <tomoakiii@gmail.com>
2025-01-31 18:54:09 +0900

    cmt

--- abc248/c.cpp
+++ abc248/c.cpp
@@ -9,30 +9,40 @@ template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, tr
 typedef long long ll;
 const ll INF = 0x0F0F0F0F0F0F0F0F;
 const int INFi = 0x0F0F0F0F;
-
+using modint998244353 mint;
 int main(){
-    ll N;
-    cin >> N;
-    vector<ll> A(N);
-    rep(i,N) cin>>A[i];
-
-    string S;
-    cin >> S;
-
-    ll N;
-    cin >> N;
-    vector<string> S(N);
-    rep(i,N) cin>>A[i];
-
-    ll N, M;
-    cin >> N >> M;
-    vector uv(N, vector<ll>{});
-    rep(i,M) {
-        int u,v;
-        cin>>u>>v;
-        u--, v--;
-        uv[u].emplace_back(v);
-        uv[v].emplace_back(u);
+    ll N, M, K;
+    cin >> N >> M >> K;
+    vector<ll> A(N, 1);
+    vector<ll> nm(M+1);
+    mint ans = 0;
+    vector<mint> ta(M+1, 1);
+    vector<mint> revta(M+1);
+    for(int i=1; i<=M; i++) {
+        ta[i] = ta[i-1];
+        ta[i] *= i;
+        revta[i] = 1 / ta[i];
     }
+    
+    auto func = [&](auto func, int cur, vector<ll> t, vector<ll> num, ll total) -> void {
+        mint tans = ta[M];
+        for(auto n: nm) {
+            tans *= revta[i];
+        }
+        ans += tans;
+        if(cur == 0) {
+            if(total < K){
+                num[t[cur]]--;
+                t[cur]++;
+                num[t[cur]]++;
+                func(func, cur, )
+            }
+        } else {
+
+        }
+    };
+    nm[1] = N;
+    func(func, 0, A, nm, N);
+    cout << ans.val() << endl;
     return 0;
 }
--- abc248/d.cpp
+++ abc248/d.cpp
@@ -9,30 +9,50 @@ template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, tr
 typedef long long ll;
 const ll INF = 0x0F0F0F0F0F0F0F0F;
 const int INFi = 0x0F0F0F0F;
-
+typedef  pair<int, int> pii;
 int main(){
-    ll N;
+    ll N, Q;
     cin >> N;
     vector<ll> A(N);
-    rep(i,N) cin>>A[i];
-
-    string S;
-    cin >> S;
-
-    ll N;
-    cin >> N;
-    vector<string> S(N);
-    rep(i,N) cin>>A[i];
-
-    ll N, M;
-    cin >> N >> M;
-    vector uv(N, vector<ll>{});
-    rep(i,M) {
-        int u,v;
-        cin>>u>>v;
-        u--, v--;
-        uv[u].emplace_back(v);
-        uv[v].emplace_back(u);
+    vector Apos(N+1, vector<int>{});
+    rep(i,N) {
+        cin>>A[i];
+        Apos[A[i]].emplace_back(i);
+    }
+    cin >> Q;
+    struct lrq{
+        int l;
+        int r;
+        int q;
+    };
+    unordered_map<ll, vector<lrq>> Qr;
+    rep(i,Q) {
+        ll l, r, x;
+        cin >> l >> r >> x;
+        l--, r--;
+        lrq tmp;
+        tmp.l = l;
+        tmp.r = r;
+        tmp.q = i;
+        Qr[x].emplace_back(tmp);
+    }
+    vector<int> ans(Q);
+    for(auto qr: Qr) {
+        vector<int> sm(N, 0);
+        int q = qr.first;
+        for(auto a: Apos[q]) {
+            sm[a] = 1;
+        }
+        rep(i, N-1) {
+            sm[i+1] += sm[i];
+        }
+        for(auto tl : qr.second){
+            if(tl.l > 0) ans[tl.q] = sm[tl.r] - sm[tl.l-1];
+            else ans[tl.q] = sm[tl.r];
+        }
+    }
+    rep(i, Q) {
+        cout << ans[i] << endl;
     }
     return 0;
 }
