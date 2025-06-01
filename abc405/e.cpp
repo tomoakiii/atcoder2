b9ee4b0321fb7624f0c2e4bea95778833742c5d1
tomoakiii <tomoakiii@gmail.com>
2025-06-01 18:48:16 +0900

    cmt

--- abc405/e.cpp
+++ abc405/e.cpp
@@ -11,40 +11,40 @@ const ll INF = 0x0F0F0F0F0F0F0F0F;
 const int INFi = 0x0F0F0F0F;
 typedef modint998244353 mint;
 
+template <class Type> class Combination{
+public:
+    vector<mint> fct{}; // array of factorial. fct[k] = k!
+    vector<mint> rfct{}; // rmfct[k] = 1/k!
+    Combination(int M){
+        fct.resize(M+1, 1);
+        rfct.resize(M+1, 1);
+        for(ll i=2; i<=M; i++) {
+            mint ip = i;
+            fct[i] = fct[i-1] * ip;
+        }
+        rfct[M] = 1/fct[M];
+        for(ll i=M; i>1; i--) {
+            mint ip = i;
+            rfct[i-1] = rfct[i] * ip;
+        }
+    }
+
+    // kCr = (k!)/(r!)/((k-r)!)
+    mint comb(ll n, ll k){
+        if (n < k || k < 0) return 0;
+        return fct[n] * rfct[k] * rfct[n-k];
+    };
+
+};
 
 int main(){
     ll A, B, C, D;
     cin>>A>>B>>C>>D;
-    ll M = 4e6;
-    vector<mint> fct(M+1,1); // array of factorization. fct[k] = k!
-    fct[1] = 1;
-    for(ll i=2; i<=M; i++) {
-        mint ip = i;
-        fct[i] = fct[i-1] * ip;
-    }
-
-    vector<mint> rfct(M+1,1); // rmfct[k] = 1/k!
-    rfct[M] = 1/fct[M];
-    for(ll i=M; i>1; i--) {
-        mint ip = i;
-        rfct[i-1] = rfct[i] * ip;
-    }
-
-    // kCr = (k!)/(r!)/((k-r)!)
-    
-    auto comb = [&](ll n, ll k)->mint{
-        if (n < k || k < 0) return 0;
-        return fct[n] * rfct[k] * rfct[n-k];
-    };
-    
-
-    map<ll, unordered_map<ll, mint>> mpp;
-    map<ll, mint> mpf;    
+    Combination<mint> CM(4e6);
     mint ans = 0;
-    //cout << comb(834150, 21994).val() << endl;
     for(ll k = 0; k<=C; k++) {
-        mint p = comb(A+k+B, B);
-        p *= comb(C-k+D-1, D-1);
+        mint p = CM.comb(A+k+B, B);
+        p *= CM.comb(C-k+D-1, D-1);
         ans += p;
     }
     
