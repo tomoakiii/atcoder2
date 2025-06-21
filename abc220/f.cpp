ae35a89f14bec0c9cb22b11efcaf8e3614f7b59d
tomoakiii <tomoakiii@gmail.com>
2025-06-21 20:48:12 +0900

    cmt

--- abc220/f.cpp
+++ abc220/f.cpp
@@ -10,22 +10,10 @@ typedef long long ll;
 const ll INF = 0x0F0F0F0F0F0F0F0F;
 const int INFi = 0x0F0F0F0F;
 
-int main(){
-    ll N;
-    cin >> N;
-    vector<ll> A(N);
-    rep(i,N) cin>>A[i];
-
-    string S;
-    cin >> S;
-
-    ll N;
-    cin >> N;
-    vector<string> S(N);
-    rep(i,N) cin>>A[i];
-
+int main(){ 
     ll N, M;
-    cin >> N >> M;
+    cin >> N;
+    M = N-1;
     vector uv(N, vector<ll>{});
     rep(i,M) {
         int u,v;
@@ -34,5 +22,40 @@ int main(){
         uv[u].emplace_back(v);
         uv[v].emplace_back(u);
     }
+    vector<int> par(N,-1);
+    vector<ll> chld(N);
+    vector<ll> node(N);
+    vector<bool> visit(N);
+    auto f = [&](auto f, int cur)->void{
+        visit[cur] = true;
+        for(auto nx: uv[cur]){
+            if(visit[nx]) continue;
+            f(f, nx);
+            chld[cur] += chld[nx];
+            node[cur] += node[nx];
+            par[nx] = cur;            
+        }
+        chld[cur] += node[cur];
+        node[cur]++;
+    };
+    f(f, 0);
+
+    vector<ll> ans(N);
+    ans[0] = chld[0];
+    vector<bool> visit2(N);
+    auto f2 = [&](auto f2, int cur)->void{
+        visit2[cur] = true;
+        if(cur != 0) {
+            ll t = ans[par[cur]] - chld[cur] - node[cur];
+            ans[cur] = chld[cur] + t + (N-node[cur]);
+        }
+        for(auto nx: uv[cur]){
+            if(visit2[nx]) continue;            
+            f2(f2, nx);
+        }
+    };
+    f2(f2,0);
+    rep(i,N) cout<<ans[i]<<endl;
+    
     return 0;
 }
