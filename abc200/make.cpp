8d0a0b9a30c6f6869faace3cab6432a593507173
tetitubuta <tomoakiii@gmail.com>
2025-07-24 21:41:15 +0900

    cmt

---
+++ abc200/make.cpp
@@ -0,0 +1,18 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+typedef long long ll;
+const ll INF = 0x0F0F0F0F0F0F0F0F;
+const int INFi = 0x0F0F0F0F;
+
+int main(){
+    cout<<200<<endl;
+    rep(i,200) cout<<(i+1)%200<<" ";
+    cout<<endl;
+    return 0;
+}
