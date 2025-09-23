052e5be393c4886eb53ee6041ef030faf1859837
tetitubuta <tomoakiii@gmail.com>
2025-09-23 17:04:29 +0900

    cmt

---
+++ abc161/b.cpp
@@ -0,0 +1,28 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+typedef long long ll;
+const ll INF = 0x0F0F0F0F0F0F0F0F;
+const int INFi = 0x0F0F0F0F;
+
+int main(){
+    ll N,M;
+    cin >> N >> M;
+    vector<ll> A(N);
+    ll cnt=0, sm=0;
+    rep(i,N) {
+        cin>>A[i];
+        sm+=A[i];
+    }
+    rep(i,N){
+        if(4*M*A[i] >= sm) cnt++;
+    }
+    if(cnt>=M) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+    return 0;
+}
