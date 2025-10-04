c6217ecb0abb3cffc6b076ed522fbb8904c7e944
tetitubuta <tomoakiii@gmail.com>
2025-10-04 12:16:57 +0900

    cmt

---
+++ abc149/c.cpp
@@ -0,0 +1,43 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+typedef long long ll;
+const ll INF = 0x0F0F0F0F0F0F0F0F;
+const int INFi = 0x0F0F0F0F;
+
+vector<ll> Eratosthenes(const ll N )
+{
+    vector<bool> is_prime( N + 1 );
+    for( ll i = 0; i <= N; i++ )
+    {
+        is_prime[ i ] = true;
+    }
+    vector<ll> P;
+    for( ll i = 2; i <= N; i++ )
+    {
+        if( is_prime[ i ] )
+        {
+            for( ll j = 2 * i; j <= N; j += i )
+            {
+                is_prime[ j ] = false;
+            }
+            P.emplace_back( i );
+        }
+    }
+    return P;
+}
+
+
+int main(){
+    ll X;
+    cin >> X;
+    auto v = Eratosthenes(10*X);
+    auto it = lower_bound(v.begin(), v.end(), X);
+    cout << *it << endl;
+    return 0;
+}
