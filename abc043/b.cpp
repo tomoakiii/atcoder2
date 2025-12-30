fef65f22c5031c217feb7612e302f4a4ce46c97a
tetitubuta <tomoakiii@gmail.com>
2025-12-30 11:55:07 +0900

    cmt

---
+++ abc043/b.cpp
@@ -0,0 +1,25 @@
+#include <atcoder/all>
+#include <bits/stdc++.h>
+using namespace std;
+using namespace atcoder;
+#define rep(i,n) for (ll i = 0; i < (n); ++i)
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+typedef long long ll;
+const ll INF = 0x0F0F0F0F0F0F0F0F;
+const int INFi = 0x0F0F0F0F;
+
+int main(){
+    string S; cin>>S;
+    string ans;
+    for(auto c:S) {
+        if(c=='B') {
+            if(!ans.empty()) ans.pop_back();
+        } else {
+            ans.push_back(c);
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
